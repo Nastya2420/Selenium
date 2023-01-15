cffd0fa6606ab794ce1abb29443435404a467dad
Anastasia <a.grebenkina93@gmail.com>
2023-01-15 17:39:06 +0300

    java

--- src/test/java/ru/netology/CardDesign.java
+++ src/test/java/ru/netology/CardDesign.java
@@ -36,15 +36,15 @@ public class CardDesign {
     }
 
     @Test
-    void shouldTest (){
+    void shouldTest() {
         driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("[data-test-id='name']input")).sendKeys("Ольга Абрамова-Иванова");
-        driver.findElement(By.cssSelector("[data-test-id='phone']input")).sendKeys("+79669999999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ольга Абрамова-Иванова");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79669999999");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
 
     }
 
